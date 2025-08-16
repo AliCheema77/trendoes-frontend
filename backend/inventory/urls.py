f793714be55022b67797413e692cb455c5aa0207
AliCheema77 <asadullahcheema77@gmail.com>
2025-08-16 23:14:08 +0500

    Removed backend code and moved next project direct inside the main folder

--- backend/inventory/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("product", views.ProductListSellView.as_view()),
-    path("product/<int:pk>", views.ProductDetailView.as_view()),
-    path("best_seller/", views.BestProductsListView.as_view()),
-]
